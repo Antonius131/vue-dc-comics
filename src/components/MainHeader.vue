fc3ef7caaa1cc76ed3cb7a20f95b986c818fa5e0
Antonius131 <antomendetta@gmail.com>
2022-07-15 15:43:15 +0200

    create navMenuItems object array

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,12 +1,65 @@
 <template>
   <header>
-      <h1>Header</h1>
+      <nav>
+         <div id="logo">
+            <img src="../assets/img/dc-logo.png" alt="DC logo">
+         </div>
+         <div id="nav-menu">
+            <ul>
+            </ul>
+         </div>
+      </nav>
   </header>
 </template>
 
 <script>
 export default {
-
+   data: function() {
+      return {
+         navMenuItems: [
+            {
+               text: 'characters',
+               link: '#', 
+            },
+            {
+               text: 'comics',
+               link: '#', 
+            },
+            {
+               text: 'movies',
+               link: '#', 
+            },
+            {
+               text: 'tv',
+               link: '#', 
+            },
+            {
+               text: 'games',
+               link: '#', 
+            },
+            {
+               text: 'collectibles',
+               link: '#', 
+            },
+            {
+               text: 'videos',
+               link: '#', 
+            },
+            {
+               text: 'fans',
+               link: '#', 
+            },
+            {
+               text: 'news',
+               link: '#', 
+            },
+            {
+               text: 'shop',
+               link: '#', 
+            },
+         ]
+      }
+   }
 }
 </script>
 
