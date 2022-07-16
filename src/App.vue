0b09cd1af2b0c896fb979d215c310808fcfedf17
Antonius131 <antomendetta@gmail.com>
2022-07-16 11:49:55 +0200

    SecondaryFooter: layout raw structure

--- src/App.vue
+++ src/App.vue
@@ -26,22 +26,23 @@ export default {
 </script>
 
 <style lang="scss">
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  // -webkit-font-smoothing: antialiased;
-  // -moz-osx-font-smoothing: grayscale;
-  // text-align: center;
-  // color: #2c3e50;
-  // margin-top: 60px;
-}
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 
-.container {
-  width: 70%;
-  margin: 0 auto;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    // -webkit-font-smoothing: antialiased;
+    // -moz-osx-font-smoothing: grayscale;
+    // text-align: center;
+    // color: #2c3e50;
+    // margin-top: 60px;
+  }
+
+  .container {
+    width: 70%;
+    margin: 0 auto;
+  }
 </style>
--- src/components/SecondaryFooter.vue
+++ src/components/SecondaryFooter.vue
@@ -1,7 +1,35 @@
 <template>
-  <footer>
-   <h1>Secondary Footer</h1>
-  </footer>
+  <div class="secondary-footer">
+    <div class="container">
+      <nav>
+          <div id="logo">
+            <button type="button" class="btn">Sign up now</button>
+          </div>
+          <div id="nav-menu">
+            <ul>
+              <li>
+                <h3>Follow us</h3>
+              </li>
+              <li>
+                <img src="../assets/img/footer-facebook.png" alt="Facebook ico">
+              </li>
+              <li>
+                <img src="../assets/img/footer-twitter.png" alt="Twitter ico">
+              </li>
+              <li>
+                <img src="../assets/img/footer-youtube.png" alt="YouTube ico">
+              </li>
+              <li>
+                <img src="../assets/img/footer-pinterest.png" alt="Pinterest ico">
+              </li>
+              <li>
+                <img src="../assets/img/footer-periscope.png" alt="Periscope ico">
+              </li>
+            </ul>
+          </div>
+      </nav>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -10,6 +38,16 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+  .secondary-footer {
+    background-color: #303030;
+    padding: .5rem 0rem;
+  }
 
+  nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem 0rem;
+  }
 </style>
